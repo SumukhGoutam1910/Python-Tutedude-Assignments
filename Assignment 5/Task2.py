8e3b03dd0d32564143364c6656970ff5d4266b11
Sumukh <sumukhgoutam0804@gmail.com>
2025-09-03 23:41:22 +0530

    Assignment 5

---
+++ Assignment 5/Task2.py
@@ -0,0 +1,5 @@
+numbers=[1,2,3,4,5,6,7,8,9,10]
+first_five=numbers[0:5]
+reversed_numbers=first_five[::-1]
+print("Extracted first five elements:", first_five)
+print("Reversed extracted elements:", reversed_numbers)
